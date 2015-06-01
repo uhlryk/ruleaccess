b150af716c1e24c0d5a24c634739fa79aefbb558
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2015-06-01 15:56:20 +0200

    add middleware and permission check in routes callbacks

--- test/tests.js
+++ test/tests.js
@@ -1,127 +1,126 @@
 var RuleAccess = require("../");
-var ruleFunctionList = require('../lib/ruleFunctionList');
+var rule = RuleAccess.rule;
 var chai = require("chai");
 var expect = chai.expect;
 
 
 describe("RuleAccess core", function(){
-	it("should return false on method isAllowed when userResource and ruleFunctionList are not set", function(){
+	it("should return false on method isAllowed when userResource and rule are not set", function(){
 		var ruleAccess = new RuleAccess();
 		expect(ruleAccess.isAllowed()).to.be.false;
 	});
-	it("should return object on method ruleFunctionList", function(){
-		var ruleAccess = new RuleAccess();
-		expect(ruleAccess.ruleFunctionList).to.be.exist;
+	it("should return object on method rule", function(){
+		expect(rule).to.be.exist;
 	});
 });
 describe("Test Default Rule Functions", function(){
 	describe("alwaysAllow", function(){
 		it("should return true when all args are undefined", function(){
-			expect(ruleFunctionList.alwaysAllow()()).to.be.true;
+			expect(rule.alwaysAllow()()).to.be.true;
 		});
 		it("should return true when args are set", function(){
-			expect(ruleFunctionList.alwaysAllow()({}, {})).to.be.true;
+			expect(rule.alwaysAllow()({}, {})).to.be.true;
 		});
 	});
 	describe("alwaysDisallow", function(){
 		it("should return false when all args are undefined", function(){
-			expect(ruleFunctionList.alwaysDisallow()()).to.be.false;
+			expect(rule.alwaysDisallow()()).to.be.false;
 		});
 		it("should return false when args are set", function(){
-			expect(ruleFunctionList.alwaysDisallow()({}, {})).to.be.false;
+			expect(rule.alwaysDisallow()({}, {})).to.be.false;
 		});
 	});
 	describe("anyOnRuleList", function(){
 		it("should return false when all args are undefined", function(){
-			expect(ruleFunctionList.anyOnRuleList()()).to.be.false;
+			expect(rule.anyOnRuleList()()).to.be.false;
 		});
 		it("should return false when one rule added : alwaysDisallow", function(){
-			expect(ruleFunctionList.anyOnRuleList([ruleFunctionList.alwaysDisallow()])({}, {})).to.be.false;
+			expect(rule.anyOnRuleList([rule.alwaysDisallow()])({}, {})).to.be.false;
 		});
 		it("should return true when one rule added : alwaysAllow", function(){
-			expect(ruleFunctionList.anyOnRuleList([ruleFunctionList.alwaysAllow()])({}, {})).to.be.true;
+			expect(rule.anyOnRuleList([rule.alwaysAllow()])({}, {})).to.be.true;
 		});
 		it("should return true when two rules added : alwaysAllow, alwaysDisallow", function(){
-			expect(ruleFunctionList.anyOnRuleList([ruleFunctionList.alwaysAllow(), ruleFunctionList.alwaysDisallow()])({}, {})).to.be.true;
+			expect(rule.anyOnRuleList([rule.alwaysAllow(), rule.alwaysDisallow()])({}, {})).to.be.true;
 		});
 		it("should return true when two rules added : alwaysDisallow, alwaysAllow", function(){
-			expect(ruleFunctionList.anyOnRuleList([ruleFunctionList.alwaysDisallow(), ruleFunctionList.alwaysAllow()])({}, {})).to.be.true;
+			expect(rule.anyOnRuleList([rule.alwaysDisallow(), rule.alwaysAllow()])({}, {})).to.be.true;
 		});
 		it("should return true when two rules added : alwaysDisallow, alwaysDisallow", function(){
-			expect(ruleFunctionList.anyOnRuleList([ruleFunctionList.alwaysDisallow(), ruleFunctionList.alwaysDisallow()])({}, {})).to.be.false;
+			expect(rule.anyOnRuleList([rule.alwaysDisallow(), rule.alwaysDisallow()])({}, {})).to.be.false;
 		});
 	});
 	describe("everyOnRuleList", function(){
 		it("should return false when all args are undefined", function(){
-			expect(ruleFunctionList.everyOnRuleList()()).to.be.false;
+			expect(rule.everyOnRuleList()()).to.be.false;
 		});
 		it("should return false when one rule added : alwaysDisallow", function(){
-			expect(ruleFunctionList.everyOnRuleList([ruleFunctionList.alwaysDisallow()])({}, {})).to.be.false;
+			expect(rule.everyOnRuleList([rule.alwaysDisallow()])({}, {})).to.be.false;
 		});
 		it("should return true when one rule added : alwaysAllow", function(){
-			expect(ruleFunctionList.everyOnRuleList([ruleFunctionList.alwaysAllow()])({}, {})).to.be.true;
+			expect(rule.everyOnRuleList([rule.alwaysAllow()])({}, {})).to.be.true;
 		});
 		it("should return false when two rules added : alwaysAllow, alwaysDisallow", function(){
-			expect(ruleFunctionList.everyOnRuleList([ruleFunctionList.alwaysAllow(), ruleFunctionList.alwaysDisallow()])({}, {})).to.be.false;
+			expect(rule.everyOnRuleList([rule.alwaysAllow(), rule.alwaysDisallow()])({}, {})).to.be.false;
 		});
 		it("should return false when two rules added : alwaysDisallow, alwaysAllow", function(){
-			expect(ruleFunctionList.everyOnRuleList([ruleFunctionList.alwaysDisallow(), ruleFunctionList.alwaysAllow()])({}, {})).to.be.false;
+			expect(rule.everyOnRuleList([rule.alwaysDisallow(), rule.alwaysAllow()])({}, {})).to.be.false;
 		});
 		it("should return false when two rules added : alwaysDisallow, alwaysDisallow", function(){
-			expect(ruleFunctionList.everyOnRuleList([ruleFunctionList.alwaysDisallow(), ruleFunctionList.alwaysDisallow()])({}, {})).to.be.false;
+			expect(rule.everyOnRuleList([rule.alwaysDisallow(), rule.alwaysDisallow()])({}, {})).to.be.false;
 		});
 		it("should return true when two rules added : alwaysAllow, alwaysAllow", function(){
-			expect(ruleFunctionList.everyOnRuleList([ruleFunctionList.alwaysAllow(), ruleFunctionList.alwaysAllow()])({}, {})).to.be.true;
+			expect(rule.everyOnRuleList([rule.alwaysAllow(), rule.alwaysAllow()])({}, {})).to.be.true;
 		});
 	});
 	describe("negateRule", function(){
 		it("should return false when all args are undefined", function(){
-			expect(ruleFunctionList.negateRule()()).to.be.false;
+			expect(rule.negateRule()()).to.be.false;
 		});
 		it("should return true when alwaysDisallow rule added", function(){
-			expect(ruleFunctionList.negateRule(ruleFunctionList.alwaysDisallow())({}, {})).to.be.true;
+			expect(rule.negateRule(rule.alwaysDisallow())({}, {})).to.be.true;
 		});
 		it("should return false when alwaysAllow rule added", function(){
-			expect(ruleFunctionList.negateRule(ruleFunctionList.alwaysAllow())({}, {})).to.be.false;
+			expect(rule.negateRule(rule.alwaysAllow())({}, {})).to.be.false;
 		});
 	});
 });
-describe("RuleAccess core when ruleFunctionList is set", function(){
+describe("RuleAccess core when rule is set", function(){
 	it("should return false on method isAllowed without resource when one rule added : alwaysAllow", function(){
 		var ruleAccess = new RuleAccess();
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysAllow());
+		ruleAccess.addRule("DummyResource", rule.alwaysAllow());
 		expect(ruleAccess.isAllowed()).to.be.false;
 	});
 	it("should return true on method isAllowed when one rule added : alwaysAllow", function(){
 		var ruleAccess = new RuleAccess();
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysAllow());
+		ruleAccess.addRule("DummyResource", rule.alwaysAllow());
 		expect(ruleAccess.isAllowed("DummyResource")).to.be.true;
 	});
 	it("should return false on method isAllowed when one rule added : alwaysDisallow", function(){
 		var ruleAccess = new RuleAccess();
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysDisallow());
+		ruleAccess.addRule("DummyResource", rule.alwaysDisallow());
 		expect(ruleAccess.isAllowed("DummyResource")).to.be.false;
 	});
 	it("should return true on method isAllowed when two rules added : alwaysAllow, alwaysAllow", function(){
 		var ruleAccess = new RuleAccess();
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysAllow());
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysAllow());
+		ruleAccess.addRule("DummyResource", rule.alwaysAllow());
+		ruleAccess.addRule("DummyResource", rule.alwaysAllow());
 		expect(ruleAccess.isAllowed("DummyResource")).to.be.true;
 	});
 	it("should return false on method isAllowed when two rules added : alwaysAllow, alwaysDisallow", function(){
 		var ruleAccess = new RuleAccess();
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysAllow());
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysDisallow());
+		ruleAccess.addRule("DummyResource", rule.alwaysAllow());
+		ruleAccess.addRule("DummyResource", rule.alwaysDisallow());
 		expect(ruleAccess.isAllowed("DummyResource")).to.be.false;
 	});
 	it("should return true on method isAllowed when rule alwaysAllow is added to tested resource, other resource has rule alwaysDisallow", function(){
 		var ruleAccess = new RuleAccess();
-		ruleAccess.addRuleResource("DummyResource", ruleFunctionList.alwaysAllow());
-		ruleAccess.addRuleResource("OtherResource", ruleFunctionList.alwaysDisallow());
+		ruleAccess.addRule("DummyResource", rule.alwaysAllow());
+		ruleAccess.addRule("OtherResource", rule.alwaysDisallow());
 		expect(ruleAccess.isAllowed("DummyResource")).to.be.true;
 	});
 });
-describe("RuleAccess when ruleFunctionList is set with custom rule", function(){
+describe("RuleAccess when rule is set with custom rule", function(){
 	it("should return true on method isAllowed when custom rule which return true is added", function(){
 		var ruleAccess = new RuleAccess();
 		function custom(){
@@ -129,7 +128,7 @@ describe("RuleAccess when ruleFunctionList is set with custom rule", function(){
 				return true;
 			};
 		}
-		ruleAccess.addRuleResource("DummyResource", custom());
+		ruleAccess.addRule("DummyResource", custom());
 		expect(ruleAccess.isAllowed("DummyResource")).to.be.true;
 	});
 	it("should return false on method isAllowed when custom rule which return false is added", function(){
@@ -139,7 +138,7 @@ describe("RuleAccess when ruleFunctionList is set with custom rule", function(){
 				return false;
 			};
 		}
-		ruleAccess.addRuleResource("DummyResource", custom());
+		ruleAccess.addRule("DummyResource", custom());
 		expect(ruleAccess.isAllowed()).to.be.false;
 	});
 });
